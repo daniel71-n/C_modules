dde327cf4caf4692fb55517991e6ee7155670b17
daniel71-n <h38c@protonmail.com>
2021-02-10 15:59:49 +0100

    Added a str_to_int() function

--- pstrings.c
+++ pstrings.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <stdlib.h>
 #include "pstrings.h"
 
@@ -23,7 +24,7 @@ unsigned int str_len(char string_arg[]){
 void str_rev(char string_to_reverse[], char string_reversed[]){
 /* Reverse string_to_reverse, writing the result to the string_reversed char array.*/
 
-    int orig_length = str_find_length(string_to_reverse);     //  get the length of 'orig'; this doesn't count the terminating NULL character
+    int orig_length = str_len(string_to_reverse);     //  get the length of 'orig'; this doesn't count the terminating NULL character
     string_reversed[orig_length] = '\0';  // all strings need to be NULL-terminated
 
     unsigned int ind = 0;
@@ -37,7 +38,7 @@ void str_rev(char string_to_reverse[], char string_reversed[]){
 void str_rev_ip(char string_arg[]){
     /* Reverse the string argument (an array of chars) IN PLACE */
     
-    unsigned int str_length = str_find_length(string_arg);  
+    unsigned int str_length = str_len(string_arg);  
     unsigned int largest_index = str_length-1;  // the length is -1 since counting starts at 0. 
     char temp; 
 
@@ -65,6 +66,41 @@ void str_rev_ip(char string_arg[]){
     }
 }
 
+
+
+int32_t str_to_int(char string_arg[]){
+    /* Convert string_arg to an integer, and return that.
+
+       string_arg must not contain non-numeric characters 
+       or be NULL, and it must be NUL-terminated.
+    */
+    int32_t res = 0;    // store the final result
+    uint8_t digit = 0;  // store each digit as it's computed
+    uint8_t sign = 0;   // if 1, add negative sign, if 0, keep positive
+    uint8_t i = 0;  // index;
+    
+    if (string_arg[0] == '-'){
+        sign = 1;   // it's a negative number
+        i = 1;  // start computing after the sign
+    }
+
+    // do this until a NUL is found, which terminates the string
+    for (; string_arg[i] != '\0'; i++){
+
+    // 7 = the offset from '0' to '7' (i.e. = '7' - '0'),
+    // since '7' is equal to '0' + 7
+        digit = string_arg[i] - '0';    
+        // each digit to the right is an order of magnitude lower than the digit to its
+        // left
+        res = res * 10 + digit;     // bump res up one order of magnitude, and add to it the next value to its right
+    }
+   
+    // negative : make the number negative
+    if (sign){
+        res = 0 - res;  // e.g. -73 is 73 below 0
+    }
+    return res;
+}
 
 
 
--- pstrings.h
+++ pstrings.h
@@ -1,5 +1,9 @@
-
+#include <stdint.h>
 
 void str_rev_ip(char string_arg[]);   // reverse string_arg in place
 void str_rev(char string_to_reverse[], char string_reversed[]);  // reverse string_arg and store the result in string_reversed
+
 unsigned int str_len(char string_arg[]);  // return the length of string_arg, not counting the terminating Nul character
+
+// convert a string to an int, provided the string doesn't contain prohibited (non-numeric) characters
+int32_t str_to_int(char string_arg[]);  
