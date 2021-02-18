3e56e19f03e4dbb35dad8f190dd188907ffdaec6
daniel71-n <h38c@protonmail.com>
2021-02-18 13:59:19 +0100

    +header guard

--- bit_utils.h
+++ bit_utils.h
@@ -1,3 +1,6 @@
+#ifndef BIT_UTILS_H
+#define BIT_UTILS_H
+
 
 #include <stdint.h>
 typedef enum signed_or_unsigned{
@@ -8,3 +11,6 @@ typedef enum signed_or_unsigned{
 void get_binary_string(char *holding_string, long long number_to_convert);
 uint8_t Count_bits(long long num);
 long long Reverse_bits(long long num);
+
+
+#endif
